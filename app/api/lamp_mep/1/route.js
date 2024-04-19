fb64ed45026effb0e7d1e95ce887cc26a9c240eb
Abdul Ghani <ghania99s.ag@gmail.com>
2024-04-19 09:54:03 +0700

    add api

---
+++ app/api/lamp_mep/1/route.js
@@ -0,0 +1,10 @@
+import { NextResponse } from 'next/server';
+
+const DATA_SOURCE_URL = 'http://192.168.10.213/1';
+
+export async function GET(request) {
+  const res = await fetch(DATA_SOURCE_URL, { cache: 'no-store' });
+  const data = await res.json();
+
+  return NextResponse.json(data);
+}
